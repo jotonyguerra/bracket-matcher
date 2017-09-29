f37074fe6e7f3b1cf0ddd167ce87fe4af09052ca
Jotony Guerra <jotonyguerra@gmail.com>
2017-09-29 15:41:27 -0700

    a bracket matcher method, uses regex

--- code.rb
+++ code.rb
@@ -1,1 +1,14 @@
 #YOUR CODE GOES HERE
+non_delimiters = /[^(){}\[\]]*/
+Paired = /\(#{non_delimiters}\)|\{#{non_delimiters}\}|\[#{non_delimiters}\]/
+Delimiter = /[(){}\[\]]/
+def bracket_match?(s)
+  s = s.dup #duplicates string
+  s.gsub!(Paired, "".freeze) while s =~ Paired
+  #copy of str with the all occurrences of pattern substituted for the second argument.
+  #The pattern is typically a Regexp;
+  s !~ Delimiter
+end
+
+
+puts bracket_match?("Hi! Wh{}at is your name {again (I forgot?)}")
